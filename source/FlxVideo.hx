94ea67d183739482e29cb16913cd8a905dc29915
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-02-21 17:06:13 +0200

    perfect

--- source/FlxVideo.hx
+++ source/FlxVideo.hx
@@ -8,30 +8,29 @@ import flixel.FlxG;
 class FlxVideo extends FlxBasic {
 	public var finishCallback:Void->Void = null;
 
+        #if android
+        var player:WebView;
+        #end
+
 	public function new(name:String) {
 		super();
 
 		#if android
-		WebView.onClose = function(){
-        	trace("WebView has been closed!");
-	        if (finishCallback != null){
+		player.onClose = function(){
+        	        trace("WebView has been closed!");
+	                if (finishCallback != null){
 				finishCallback();
 			}
 		}
-		WebView.onURLChanging = function(url:String){
-	        trace("WebView is about to open: " + url);
-	        if (url == 'http://exitme/') 
-			{
-	        	if (finishCallback != null){
+		player.onURLChanging = function(url:String){
+	                trace("WebView is about to open: " + url);
+	                if (url == 'http://exitme'){
+	        	        if (finishCallback != null){
 					finishCallback();
 				}
 			}
 		}
-		WebView.open(AndroidTools.getFileUrl(name), null, ['http://exitme(.*)']);
-		#else
-		if (finishCallback != null){
-				finishCallback();
-			}
+		player.open(AndroidTools.getFileUrl(name), null, ['http://exitme']);
 		#end
 	}
 }
