b52621284ddbde20465e70becb83972d746076ae
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-02-21 12:16:47 +0200

    Videos Stuff

---
+++ source/FlxVideo.hx
@@ -0,0 +1,37 @@
+#if android
+import extension.webview.WebView;
+import android.*;
+#end
+import flixel.FlxBasic;
+import flixel.FlxG;
+
+class FlxVideo extends FlxBasic {
+	public var finishCallback:Void->Void = null;
+
+	public function new(name:String) {
+		super();
+
+		#if android
+		WebView.onClose = function(){
+        	trace("WebView has been closed!");
+	        if (finishCallback != null){
+				finishCallback();
+			}
+		}
+		WebView.onURLChanging = function(url:String){
+	        trace("WebView is about to open: " + url);
+	        if (url == 'http://exitme/') 
+			{
+	        	if (finishCallback != null){
+					finishCallback();
+				}
+			}
+		}
+		WebView.open(AndroidTools.getFileUrl(name), null, ['http://exitme(.*)']);
+        #else
+    	if (finishCallback != null){
+			finishCallback();
+		}
+        #end
+	}
+}
